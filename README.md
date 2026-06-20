13ccd9bba6ad13061d826398d03d82aee38b594d
Mehrdad <jvjehrdad@gmail.com>
2026-06-20 19:51:15 +0330

    add roman to integer solution in js

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ My LeetCode solutions in JavaScript.
 |---|---------|------------|----------|
 | 1 | [Two Sum](https://leetcode.com/problems/two-sum/) | Easy | [two-sum.js](two-sum.js) |
 | 9 | [Palindrome Number](https://leetcode.com/problems/palindrome-number/) | Easy | [palindrome-number.js](palindrome-number.js) |
+| 13 | [Roman to Integer](https://leetcode.com/problems/roman-to-integer/) | Easy | [Roman-to-Integer.js](Roman-to-Integer.js) |
---
+++ Roman-to-Integer.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  let total = 0;
+
+  const convertRomanToNumber = (char) => {
+    switch (char) {
+      case 'I':
+        return 1;
+      case 'V':
+        return 5;
+      case 'X':
+        return 10;
+      case 'L':
+        return 50;
+      case 'C':
+        return 100;
+      case 'D':
+        return 500;
+      case 'M':
+        return 1000;
+      default:
+        return 0;
+    }
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    const current = convertRomanToNumber(s[i]);
+    const next = convertRomanToNumber(s[i + 1]);
+
+    if (current < next) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+
+  return total;
+};
